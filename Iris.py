8dd7cdf13dbd7a18167f82235e441bb752c29f83
Freeha-S <freha.gretb@gmail.com>
2019-05-10 12:22:04 +0100

    first commit

---
+++ Iris.py
@@ -0,0 +1,6 @@
+# Making a new Iris project repository to improve my data Analytical skill and python Programming
+# try to use numpy and skilearn
+import pandas as  pd
+import numpy as np
+import matplotlib as mp
+import seaborn as sn
